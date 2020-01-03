1ba724740036a6927b5ec581af55dc23ee2c7e77
Matthew Short <short.matthew.f@gmail.com>
2020-01-03 16:56:51 -0500

    db

---
+++ seed.js
@@ -0,0 +1,50 @@
+const faker = require('faker')
+
+const USERS = []
+let currentUserId = 0
+
+const makeUser = () => ({
+  id: currentUserId++,
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.hacker.verb() + Math.floor(1000 + 9000 * Math.random()),
+  avatarImg: faker.internet.avatar()
+})
+
+const makeUsers = () => {
+  for(let i = 0; i < 50; i++) {
+    USERS.push(makeUser())
+  }
+}
+
+makeUsers()
+
+const POSTS = []
+let currentPostId = 0
+
+const makePost = (userId) => ({
+  id: currentPostId++,
+  userId,
+  title: faker.hacker.phrase(),
+  content: [
+    faker.company.catchPhrase(),
+    faker.company.catchPhrase()
+  ]
+})
+
+const makePosts = () => {
+  USERS.forEach(user => {
+    for(let i = 0; i < 3 + Math.floor(8 * Math.random()); i++) {
+      POSTS.push(makePost(user.id))
+    }
+  })
+}
+
+makePosts()
+
+const fs = require('fs')
+
+fs.writeFileSync('./db.json', JSON.stringify({
+  users: USERS,
+  posts: POSTS
+}))
